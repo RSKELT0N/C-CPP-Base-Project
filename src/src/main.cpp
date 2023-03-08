41dc9b2344ff64f9e663e75f72eff442aa08a832
Ryan Skelton <ryanskelton2000@yahoo.com>
2023-03-08 15:51:29 +0000

    Remove unneeded include directories.

--- src/src/main.cpp
+++ src/src/main.cpp
@@ -1,8 +1,20 @@
-#include <fmt/core.h>
 #include <fmt/color.h>
+
+template<int i>
+struct fact {
+    static constexpr int value = i * fact<i-1>::value;
+};
+
+template<>
+struct fact<0> {
+    static constexpr int value = 1;
+};
+
 
 int main([[maybe_unused]]int argc, [[maybe_unused]]char** argv) {
 
-    fmt::print(fg(fmt::color::crimson), "Base-Project\n");
+    static constexpr int x = 5;
+    fmt::print("Val: {}", fact<x>::value);
+
     return 0;
 }
